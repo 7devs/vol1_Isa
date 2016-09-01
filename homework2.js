88a868610f3efb34f8a2d7d25853027c1fb8e4b8
Isa <isazhang15@163.com>
2016-09-01 16:01:29 +0800

    trytrytry always try

--- homework2.js
+++ homework2.js
@@ -6,13 +6,11 @@ person.age = 27;
 
 
 var fruit = 'apple';
-if (fruit === 'pear'){
-    console.log('This is not an apple, it's a pear);
+if (fruit === 'pear') {
+    console.log('This is not an apple, it'\s a pear.');
     console.log('Try another one?');
-} else if (fruit === 'Apple'){
-    console.log('This is an apple with capital A');
 } else if (fruit === 'apple'){
-    console.log('This is an apple with lowercase a');
-} else{
+    console.log('This is an apple.');
+} else {
     console.log('Try another fruit name.');
 }
