5ae87d8301e25933f231937c58a1800689af1b12
Isa Zhang <isazhang15@163.com>
2016-09-01 14:29:20 +0800

    hard to push online dont know why

--- homework2.js
+++ homework2.js
@@ -3,3 +3,16 @@ person.first_name = 'Isa';
 person.last_name = 'Zhang';
 person.gender = 'female';
 person.age = 27;
+
+
+var fruit = 'apple';
+if (fruit = 'pear'){
+    console.log('This is not an apple, it's a pear);
+    console.log('Try another one?');
+} else if (fruit = 'Apple'){
+    console.log('This is an apple with capital A');
+} else if (fruit = 'apple'){
+    console.log('This is an apple with lowercase a')
+} else{
+    console.log('Try another fruit name.')
+}
