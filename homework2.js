5d3771ffed63eea841c5f2ea4b9f653423602f61
Isa Zhang <isazhang15@163.com>
2016-09-01 14:43:03 +0800

    edit for errors

--- homework2.js
+++ homework2.js
@@ -6,13 +6,13 @@ person.age = 27;
 
 
 var fruit = 'apple';
-if (fruit = 'pear'){
+if (fruit === 'pear'){
     console.log('This is not an apple, it's a pear);
     console.log('Try another one?');
-} else if (fruit = 'Apple'){
+} else if (fruit === 'Apple'){
     console.log('This is an apple with capital A');
-} else if (fruit = 'apple'){
-    console.log('This is an apple with lowercase a')
+} else if (fruit === 'apple'){
+    console.log('This is an apple with lowercase a');
 } else{
-    console.log('Try another fruit name.')
+    console.log('Try another fruit name.');
 }
