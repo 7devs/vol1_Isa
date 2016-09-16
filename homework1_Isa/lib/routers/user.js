d12b33bb84f24fbcbcaaa0a03bf940da76e98c5a
Isa Zhang <isazhang15@163.com>
2016-09-16 10:33:34 +0800

    notes

--- homework1_Isa/lib/routers/user.js
+++ homework1_Isa/lib/routers/user.js
@@ -20,23 +20,35 @@ router.route('/:id').get(function(req,res,next){
 });
 
 router.route('/:id').put(function(req, res, next) {
-var id = req.params.id;
+var id = req.params.id-1;
 var ageinput = req.query.age;
 var ageNumber = parseInt(ageinput);
 
-if (userModel[id-1]){
+
+/* 第一种提交if从句的方法，试验已成功
+if (userModel[id]) {
 
   if (ageNumber){
-  userModel[id-1].age = ageNumber;
-  res.status(200).send(userModel[id-1]);
+  userModel[id].age = ageNumber;
+  res.status(200).send(userModel[id]);
   } else {
-  res.status(403).send('这不是数字。');
+  res.status(403).send('需要修改age,并提交数字。');
   }
 
-}else {
+} else {
   res.status(404).send('Post.没有这个ID');
+}
+});
+*/
+
+if (userModel[id] && ageNumber) {
+  userModel[id].age = ageNumber;
+  res.status(200).send(userModel[id]);
+} else if (userModel[id] || ageNumber) {
+  res.status(403).send('请检查ID是否存在，及年龄是否是数字');
+} else {
+  res.status(403).send('这个判断方式没有上面那个好。');
 }
 
 });
-
 module.exports = router;
